8628f835dba6e160d4a622bf24f0636d7b7925af
ankitpal610 <ankitpal092501@gmail.com>
2025-08-28 11:58:14 +0530

    changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert(3 + 3)    we are using node js, not browser
+
+
+
+console.log(3 
+    +
+     3) // code readibility should be high
+
+console.log("ankit");
+     
+
+
+let name = " ankit "
+let age = 18
+let isLoggedIn = true
+
+
+// number -- 2 to the power 53
+// bigint
+// string = ""
+// bolean = true/false
+// null = standalone values
+// undefined 
+// symbol = unique values
+
+
+
+//  object
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+
