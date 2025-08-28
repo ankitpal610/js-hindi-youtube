fd12609e294cb3dc3c55b91fc1689e3ff75c545a
ankitpal610 <ankitpal092501@gmail.com>
2025-08-28 10:50:25 +0530

    changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "ankit123@gmail.com"
+var accountPassword = "84774"
+account_city = "jaipur"
+let accountState;
+
+accountEmail = "sjdls@";
+accountPassword= "98765";
+account_city = "delhi";
+
+/* 
+  prefer not use of var 
+  because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,account_city,accountState])
