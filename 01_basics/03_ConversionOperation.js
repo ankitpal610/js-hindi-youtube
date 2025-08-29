2e9c4628183130efbb37658f2a2acd780a4cafa3
ankitpal610 <ankitpal092501@gmail.com>
2025-08-29 17:28:34 +0530

    changes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,34 +1,34 @@
-// let score = null //undefined, null, boolean
+let score = null //undefined, null, boolean
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // Note "33" = 33
 // "33abc" = NaN; "abc"--NaN
 // true--1; false--0
 
 
-// let isLoggedIn = 1
+let isLoggedIn = 1
 
-// let boolleanIsLogged = Boolean(isLoggedIn)
-// console.log(typeof boolleanIsLogged);
-// console.log(boolleanIsLogged);
+let boolleanIsLogged = Boolean(isLoggedIn)
+console.log(typeof boolleanIsLogged);
+console.log(boolleanIsLogged);
 
 // Note 1 = true; 0 = false;
 // "" = false;
 //  "hitesh" = true;
 
 
-// let someNumber = 33
+let someNumber = 33
 
-// let stringNumber = String(someNumber)
-// console.log(typeof stringNumber);
-// console.log(stringNumber);
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
 
 
 
