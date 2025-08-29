d6dd5a820fbbc7144032e2f57d55ea3ed13b171f
ankitpal610 <ankitpal092501@gmail.com>
2025-08-29 13:59:40 +0530

    chnages

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,34 +1,79 @@
-let score = null //undefined, null, boolean
+// let score = null //undefined, null, boolean
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // Note "33" = 33
 // "33abc" = NaN; "abc"--NaN
 // true--1; false--0
 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let boolleanIsLogged = Boolean(isLoggedIn)
-console.log(typeof boolleanIsLogged);
-console.log(boolleanIsLogged);
+// let boolleanIsLogged = Boolean(isLoggedIn)
+// console.log(typeof boolleanIsLogged);
+// console.log(boolleanIsLogged);
 
 // Note 1 = true; 0 = false;
 // "" = false;
 //  "hitesh" = true;
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+
+// **********************---OPERATION---****************************//
+
+let value = 3
+let negValue = -3
+
+console.log(negValue);
+console.table([2+2,2-2,2*2,2**3,2/2,2%3]);
+
+let str1 = "hellow"
+let str2 = " hitesh"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "4");
+
+
+
+
+console.log(true);
+console.log(+true);
+
+console.log(+"");
+
+
+console.log(false);
+console.log(+false);
+
+
+let gamecounter = 100
+console.log(++gamecounter);
+console.log(++gamecounter);
+
+
+
+
+
+
 
 
 
