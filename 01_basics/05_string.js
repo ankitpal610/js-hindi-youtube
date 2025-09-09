c24b917bec065eca0d4b86a0ef2ca5d3f8664f02
ankitpal610 <palankit2527@gmail.com>
2025-09-09 09:18:54 +0530

    now half we add in string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+const name = "ankit"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+
+console.log(`hello my name is ${name} and my name repo count is ${repocount}`);
+
+
+const gamename = new String('hitesh-hc')
+ 
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('t'));
+
+const newString = gamename.substring(0,4);
+console.log(newString);
+
+const anotherString = gamename.slice(-8,4)
+console.log(anotherString);
+
+
+const newStringone = "     ankit    "
+console.log(newStringone);
+console.log(newStringone.trim());
