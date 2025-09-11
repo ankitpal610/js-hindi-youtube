99e5b6cee5f7b6a4894a3920d86b78d48add9e24
ankitpal610 <palankit2527@gmail.com>
2025-09-11 19:39:06 +0530

    scope and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,53 @@ if(true){
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log(a);
+    // console.log(a);
     
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// next video 
+
+function one(){
+    const username = "ankit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+    two();
+}
+
+// one();
+
+
+if(true){
+    const username = "ankit"
+    if(username === "ankit"){
+        const website = "  youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++++== Intersting  ==+++++++++++++++++++++++++++
+
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5);
+const addtwo = function(num){   // js ke andar variable kafi powerfull hoti kafi kuchh hold krti h like that json value, function etc
+
+    return num + 2
+
+}
