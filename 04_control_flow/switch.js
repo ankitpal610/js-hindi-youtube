b0cc854dd6537633c070dc9d5e93f0e1e4e8439c
ankitpal610 <palankit2527@gmail.com>
2025-09-15 12:41:55 +0530

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = "april"  // 2,4,5,
+switch (month) {
+    case "january":  // 2
+        console.log("january");
+        break;
+
+    case "february": //3
+    console.log("february");
+    break;
+    case "march":  // 4
+        console.log("march");
+        break;
+    case "april":  // 5
+        console.log("april");
+        break;
+
+
+    default:
+        break;
+}
