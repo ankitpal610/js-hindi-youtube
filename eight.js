242b8d842785b42823d80608075f50388f2d07df
ankitpal610 <palankit2527@gmail.com>
2025-09-17 09:49:27 +0530

    changes

--- eight.js
+++ eight.js
@@ -1,39 +1,14 @@
-const myNums = [1,2,3]
+const myNumber = [1,2,3,4,5,6,7,8,9,10]
 
-// const mytotal = myNums.reduce(function (acc, currval){
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-    
-// },0)            // yha pr readibility high h
-
-// console.log(mytotal);
-
-const mytotal = myNums.reduce( (acc, currval) => acc+currval,0)
-// yha pr smartness h
-console.log(mytotal);
+// const newNums = myNumber.map( (num) => {
+//     return num + 10   // ye value ko return krta h
+// })
+// console.log(newNums);
 
 
+const newNums = myNumber
+                        .map( ( nums) => nums*10 )
+                        .map((sum) => sum + 1)
+                        .filter( (num) => num >=40)
 
-const shoppingCart = [
-    {
-        itemName : "js course",
-        price: 2999
-    },
-    {
-        itemName : "py course",
-        price: 999
-    },
-    {
-        itemName : "data science course",
-        price: 5999
-    },
-    {
-        itemName : "mobile dev course",
-        price: 12999
-    },
-
-]
-
-const priceToPay = shoppingCart.reduce((acc,item) => acc + item.price, 0)
-
-console.log(priceToPay);
+console.log(newNums);
